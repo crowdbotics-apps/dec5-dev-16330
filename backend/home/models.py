ded0c8fb4e81e1df921d01c91f91f30f4b9dc2e5
Crowdbotics <team@crowdbotics.com>
2020-12-05 12:25:29 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -33,6 +33,28 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     body = models.TextField()
@@ -40,6 +62,26 @@ class HomePage(models.Model):
         null=True,
         blank=True,
     )
+    gjhg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
